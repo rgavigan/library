40eeb6fa36babc312e80eb50540bbf591d3959fc
rgavigan <rileygav@hotmail.com>
2022-03-18 08:44:42 -0400

    Foundation

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function that takes user input and stores new book objects into an array
+let myLibrary = []; // Set library to empty array
+
+function Book() {
+
+}
+
+function addBookToLibrary () {
+    
+}
