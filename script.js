75facfd6a6b03a5d5dde8601798164178a2c0c2c
rgavigan <rileygav@hotmail.com>
2022-03-18 11:00:10 -0400

    more framework

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Function that takes user input and stores new book objects into an array
 let myLibrary = []; // Set library to empty array
 
-function Book() {
-
+function Book(title, author, numberPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberPages = numberPages;
+    this.read = read;
 }
 
 function addBookToLibrary () {
